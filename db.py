10f864e640fad4700e48fd71054f69891611c61b
Brian Gomez <briangx92@gmail.com>
2019-03-30 10:07:24 -0400

    deleted db.py, conftest.py, test_app.py, package lock trying to send the information to a database in postgresql trying to get the list of todo's to stay

--- db.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect("dbname=Flask-Todo user=admin password=password host=localhost")
-
-cur = conn.cursor()
--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,63 +1,38 @@
-from flask import Flask, request, make_response, render_template, redirect, url_for
-from flask_todo.todo import Todos
+from flask import Flask, request, render_template
+from datetime  import datetime 
 import psycopg2
 
-conn=psycopg2.connect("dbname='Flask-Todo' host='localhost'")
-try:
-    conn=psycopg2.connect("dbname='Flask-Todo' host='localhost'")
-    print("connection successful")
-except:
-    print("I am unable to connect to the database.")
-    
-cur = conn.cursor()
-cur.execute("""CREATE TABLE test (test varchar(30)); """)
-try:
-    cur.execute("""DROP TABLE task""")
-    print("table dropped")
-except:
-    print("I can't drop our test database!")
-# rows = cur.fetchall()
-# print("\nShow me the databases:\n")
-# for row in rows:
-#     print("   ", row[0])
 
-def create_app(test_config=None): #Factory app
+
+
+def create_app(): #Factory app
     app = Flask(__name__, instance_relative_config=True) #instantiating object
 
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    conn = psycopg2.connect("dbname='Flask-Todo' host='localhost'")
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    cur = conn.cursor()
 
-    @app.route('/')
-    def index(): 
-       return render_template('index.html')
-
-    task1 = Todos(name='Grocery', description='Get milk')
-    task2 = Todos(name='Clean', description='Clean room')
+    cur.execute("CREATE TABLE task (task varchar(30), description varchar(100), created varchar(30));")
     
-    task_list = [task1, task2]
-
-    @app.route('/todo', methods=['GET', 'POST'])
-    def todo():
-        # result = None
-        # if request.method == 'GET':
-        #     return render_template('todo.html')
-        # elif request.method == 'POST':
-        #     task = request.form['task']
-        #     description = request.form['description']
-        #     action = request.form['action']
-
-        #     if action == "task":
-
-            
-        #     return render_template('todo.html', task=task, description=description, action=action)
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
         
-        return render_template('todo.html', task_list=task_list) 
+        task_date = datetime.now()
 
 
+        if request.method == 'POST':
+            req_task = request.form['task']
+            description = request.form['description']
+            task_dict = [{'task': req_task, 'description': description, 'time': task_date}]
+            return render_template('todo.html',task_dict=task_dict, task_date=task_date)
+
+        
+        
+
+
+        return render_template('index.html')
+
+    
     return app
+
+
