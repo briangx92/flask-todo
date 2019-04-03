b73ce802bca30abecb3f50583dcd0d66dd87b743
Brian Gomez <briangx92@gmail.com>
2019-04-03 15:47:14 -0400

    heroku installation

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flask_todo.wsgi --log-file -
--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -2,21 +2,17 @@ from flask import Flask, request, render_template, make_response
 from datetime  import datetime
 import psycopg2
 
-
-
-
 def create_app(): #Factory app
     app = Flask(__name__, instance_relative_config=True) #instantiating object
-
-    conn = psycopg2.connect("dbname='Flask-Todo' host='localhost'")
+    
+    conn = psycopg2.connect("dbname=Flask-Todo user=csetuser")
 
     cur = conn.cursor()
 
-    cur.execute("CREATE TABLE todos (task varchar(30), description varchar(100), created varchar(30));")
+    # cur.execute("CREATE TABLE todos (task varchar(30), description varchar(100), created varchar(30));")
     
     @app.route('/', methods=['GET', 'POST'])
     def index():
-        
         task_date = datetime.today()
 
         if request.method == 'POST':
@@ -24,11 +20,11 @@ def create_app(): #Factory app
             description = request.form['description']
             task_dict = [{'task': task, 'description': description, 'time': task_date, 'complete': False}]
             return render_template('index.html',task_dict=task_dict, task_date=task_date)
+        
         elif request.method == 'GET':
             return render_template('index.html')
-        
 
-        cur.execute("INSERT INTO todos (task, created, complete) VALUES (%s,%s,%s)", (task, task_date, False))
+        cur.execute("INSERT INTO todos (task, description, created, complete) VALUES (%s,%s,%s,%s)", ("task", "description", "task_date", "False"))
         conn.commit()
         cur.close()
         conn.close()
